c55e8f3cb25aa028313aee262c8511f9c0ba50b0
local aknath <aknathcreative@gmail.com>
2024-03-03 23:38:33 -0600

    update cmakelist for CI/CD

--- HW1/build/CTestTestfile.cmake
+++ HW1/build/CTestTestfile.cmake
@@ -4,3 +4,5 @@
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
+add_test(OutputTest "python3" "../Test/test_output.py")
+set_tests_properties(OutputTest PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/natha/Documents/C_C++_project/Data_Structures_C_PP/HW1/CMakeLists.txt;7;add_test;C:/Users/natha/Documents/C_C++_project/Data_Structures_C_PP/HW1/CMakeLists.txt;0;")
